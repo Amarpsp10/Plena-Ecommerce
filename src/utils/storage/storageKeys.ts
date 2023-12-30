9da2c4886ab6178033a70aae34fd8be2451dc742
Amar Preet Singh <amarpreet@untether.tech>
2023-12-30 23:24:31 +0530

    async storage support added

---
+++ src/utils/storage/storageKeys.ts
@@ -0,0 +1,51 @@
+//Auth keys for local storage
+const refreshToken = 'refreshToken';
+const accessToken = 'token';
+const tokenExpiresIn = 'tokenExpiresIn';
+const tokenCreatedAt = 'tokenCreatedAt';
+const user = 'user';
+
+// setting keys
+const promptNextTask = 'promptNextTask';
+const callSummaryEnabled = 'callSummaryEnabled';
+const callerIdEnabled = 'callerIdEnabled';
+const recordingPath = 'recordingPath';
+const autoAttachRecording = 'recordingEnabled';
+const autoCreateContacts = 'autoCreateContacts';
+const autoCreateLogs = 'autoCreateLogs';
+const selectedSim = 'selectedSim';
+const selectedWorkspaceForAutoCreateContact =
+  'selectedWorkspaceForAutoCreateContact';
+
+//workspace
+const workspace = 'workspace';
+const allWorkspaces = 'allWorkspaces';
+
+// other
+const locationSearchData = 'locationSearchData';
+const locationSearchHistory = 'locationSearchHistory';
+
+// Global search
+const globalSearchHistory = 'globalSearchHistory';
+
+export const storageKeys = {
+  refreshToken,
+  accessToken,
+  tokenExpiresIn,
+  tokenCreatedAt,
+  user,
+  workspace,
+  allWorkspaces,
+  promptNextTask,
+  callSummaryEnabled,
+  callerIdEnabled,
+  recordingPath,
+  autoAttachRecording,
+  autoCreateContacts,
+  autoCreateLogs,
+  selectedSim,
+  selectedWorkspaceForAutoCreateContact,
+  locationSearchData,
+  locationSearchHistory,
+  globalSearchHistory,
+};
